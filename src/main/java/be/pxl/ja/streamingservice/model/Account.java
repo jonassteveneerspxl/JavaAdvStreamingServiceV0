f49b9b427cf8c84f7a133e24faad8d7b17530c35
jonassteveneerspxl <jonas.steveneers@student.pxl.be>
2020-10-02 08:25:45 +0200

    afwerking H3

--- src/main/java/be/pxl/ja/streamingservice/model/Account.java
+++ src/main/java/be/pxl/ja/streamingservice/model/Account.java
@@ -5,9 +5,35 @@ import java.util.List;
 public class Account {
     private String email;
     private String password;
-    private Paymentinfo paymentinfo;
+    private PaymentInfo paymentinfo;
     private StreamingPlan streamingPlan;
     private List<Profile> profiles;
 
+    public void setStreamingPlan(StreamingPlan streamingPlan) {
+        this.streamingPlan = streamingPlan;
+    }
 
+    public void addProfile(Profile profile){
+        profiles.add(profile);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean verifyPassword(String password) {
+        return true;
+    }
+
+    public void setPaymentinfo(PaymentInfo paymentinfo) {
+        this.paymentinfo = paymentinfo;
+    }
+
+    public Profile getFirstProfile(){
+        return profiles.get(0);
+    }
 }
